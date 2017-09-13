f2a504c33027b5fbac0ff9a71ac8dd058159b96f
nguyenmanhdong <mdong151@gmail.com>
2017-09-13 23:17:53 +0700

    adding Selenium java 3.5.3 into pom.xml Initialize Commonfunctions class

--- seleniumfw/src/main/java/modules/BMITest.java
+++ seleniumfw/src/main/java/modules/BMITest.java
@@ -1,5 +1,8 @@
 package modules;
 
 public class BMITest {
-
+	public static void main(String[] arg){
+		System.out.println(System.getProperty("user.dir"));
+	}
 }
+
--- seleniumfw/src/main/java/supports/CommonFunctions.java
+++ seleniumfw/src/main/java/supports/CommonFunctions.java
@@ -1,5 +1,26 @@
 package supports;
 
-public class CommonFunctions {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class CommonFunctions {
+	private static WebDriver driver; 
+	private static String filePath = System.getProperty("user.dir") ;
+	
+	public static void setBrowser(String browser){
+		if (browser.equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", filePath +"/Drivers/");
+		}
+		else if(browser.equalsIgnoreCase("firefox")){
+			System.setProperty("webdriver.gecko.driver", filePath+"/Drivers/");
+		}
+	}
+	public static WebElement findElement(String how, String locator){
+		WebElement ele = null;
+		
+		
+		return ele;
+		
+	}
+	
 }
