74fffb818242b21f44eb7aeb610cfff5f50bee4c
nguyenmanhdong <mdong151@gmail.com>
2017-09-14 06:38:01 +0700

    Add some method in commonFunctions

--- seleniumfw/src/main/java/modules/BMITest.java
+++ seleniumfw/src/main/java/modules/BMITest.java
@@ -3,6 +3,7 @@ package modules;
 public class BMITest {
 	public static void main(String[] arg){
 		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("os.name"));
 	}
 }
 
--- seleniumfw/src/main/java/supports/CommonFunctions.java
+++ seleniumfw/src/main/java/supports/CommonFunctions.java
@@ -1,26 +1,68 @@
 package supports;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CommonFunctions {
 	private static WebDriver driver; 
 	private static String filePath = System.getProperty("user.dir") ;
 	
 	public static void setBrowser(String browser){
-		if (browser.equalsIgnoreCase("chrome")){
-			System.setProperty("webdriver.chrome.driver", filePath +"/Drivers/");
+		if(System.getProperty("os.name").contains("Mac")){
+			if (browser.equalsIgnoreCase("chrome")){
+				System.setProperty("webdriver.chrome.driver", filePath +"/Drivers/");
+				driver = new ChromeDriver();
+			}
+			else if(browser.equalsIgnoreCase("firefox")){
+				System.setProperty("webdriver.gecko.driver", filePath+"/Drivers/");
+				driver = new FirefoxDriver();
+			}
 		}
-		else if(browser.equalsIgnoreCase("firefox")){
-			System.setProperty("webdriver.gecko.driver", filePath+"/Drivers/");
+		if(System.getProperty("os.name").contains("Windows")){
+			if (browser.equalsIgnoreCase("chrome")){
+				System.setProperty("webdriver.chrome.driver", filePath +"/Drivers/");
+				driver = new ChromeDriver();
+			}
+			else if(browser.equalsIgnoreCase("firefox")){
+				System.setProperty("webdriver.gecko.driver", filePath+"/Drivers/");
+				driver = new FirefoxDriver();
+			}
 		}
+		
 	}
 	public static WebElement findElement(String how, String locator){
 		WebElement ele = null;
-		
+		if (how.equalsIgnoreCase("id")){
+			ele = driver.findElement(By.id(locator));
+		}
+		else if (how.equalsIgnoreCase("name")){
+			ele = driver.findElement(By.name(locator));
+		}
+		else if (how.equalsIgnoreCase("xpath")){
+			ele = driver.findElement(By.xpath(locator));
+		}
+		else if (how.equalsIgnoreCase("css")){
+			ele = driver.findElement(By.cssSelector(locator));
+		}
+		else if (how.equalsIgnoreCase("class")){
+			ele = driver.findElement(By.className(locator));
+		}
 		
 		return ele;
-		
 	}
 	
+	public static void click(String how, String locator){
+		findElement(how, locator).click();
+	}
+	
+	public static void click (WebElement ele){
+		ele.click();
+	}
+	
+	public static void sendText(String how, String locator){
+		
+	}
 }
